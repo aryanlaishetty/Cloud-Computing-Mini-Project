f41c7bdf66f9b330e424697cc899747f5c976319
Om <omnavale128@gmail.com>
2026-03-10 17:56:39 +0530

    Songs Added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const playButton = document.getElementById("play");
 const todoItemsContainer = document.getElementById('todolist');
 const addTodoButton = document.getElementById('addTodoButton');
 const saveTodoButton = document.getElementById('saveTodoButton');
+const body = document.getElementById('body');
+const audioPlayer = document.getElementById("audioPlayer");
 
 const img = document.createElement('img');
 img.src = 'https://assets.ccbp.in/frontend/react-js/nxt-watch-no-saved-videos-img.png';
@@ -10,14 +12,14 @@ img.classList.add('no-img')
 let song = "play";
 
 function play() {
-    if (song === 'play') {
-        song = 'pause';
-        playButton.classList.remove('fa-play');
-        playButton.classList.toggle("fa-pause");
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        document.getElementById("play").classList.remove("fa-play");
+        document.getElementById("play").classList.add("fa-pause");
     } else {
-        song = 'play';
-        playButton.classList.remove('fa-pause');
-        playButton.classList.toggle("fa-play");
+        audioPlayer.pause();
+        document.getElementById("play").classList.remove("fa-pause");
+        document.getElementById("play").classList.add("fa-play");
     }
 }
 
@@ -170,8 +172,11 @@ const minuties = document.getElementById('min')
 const sec = document.getElementById('sec')
 let min = document.getElementById('min').textContent
 let seconds = document.getElementById('sec').textContent
-let id;
+let id = null;
 function startTimer() {
+    if(id !== null) {
+        return;
+    }
     id = setInterval(() => {
         if(seconds === "00") {
         seconds = 59
@@ -193,4 +198,68 @@ function startTimer() {
 }
 function stopTimer() {
     clearInterval(id)
+    id = null;
 }
+function resetTimer() {
+    clearInterval(id)
+    id = null;
+    min = 25
+    seconds = '00'
+    minuties.textContent = min
+    sec.textContent = seconds
+}
+
+const gifs = ["gifs/1.gif", "gifs/2.gif", "gifs/3.gif", "gifs/4.gif", "gifs/5.gif", "gifs/6.gif", "gifs/7.gif", "gifs/8.gif", "gifs/9.gif", "gifs/10.gif", "gifs/11.gif", "gifs/12.gif", "gifs/13.gif", "gifs/14.gif"];
+const songs = [
+    { name: "Rain Lofi", file: "songs/song1.mp3" },
+    { name: "Night Study", file: "songs/song2.mp3" },
+    { name: "Chill Beats", file: "songs/song3.mp3" }
+];
+
+let currentSong = 0;
+const songName = document.querySelector(".song-name");
+
+function loadSong(index) {
+    audioPlayer.src = songs[index].file;
+    songName.innerText = songs[index].name;
+}
+loadSong(currentSong);
+
+function next() {
+    const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
+    body.style.backgroundImage = `url(${randomGif})`;
+    currentSong++;
+
+    if (currentSong >= songs.length) {
+        currentSong = 0;
+    }
+
+    loadSong(currentSong);
+    audioPlayer.play();
+}
+audioPlayer.addEventListener("ended", () => {
+    next();
+});
+
+const muteBtn = document.querySelector(".mute-sound");
+const muteIcon = document.querySelector(".mute-sound i");
+
+muteBtn.addEventListener("click", () => {
+
+    audioPlayer.muted = !audioPlayer.muted;
+
+    if (audioPlayer.muted) {
+        muteIcon.classList.remove("fa-volume-high");
+        muteIcon.classList.add("fa-volume-xmark");
+    } else {
+        muteIcon.classList.remove("fa-volume-xmark");
+        muteIcon.classList.add("fa-volume-high");
+    }
+
+});
+
+const volumeSlider = document.querySelector(".volume-slider");
+
+volumeSlider.addEventListener("input", () => {
+    audioPlayer.volume = volumeSlider.value;
+});
